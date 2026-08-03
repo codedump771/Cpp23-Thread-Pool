d76a06c448aa5388d7135d1a9b4d3524ae2c512e
PantuflasDev <261576178+PantuflasDev@users.noreply.github.com>
2026-08-03 19:03:54 +0200

    Add enqueue_detach function

--- thread_pool.hpp
+++ thread_pool.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <future>
 #include <mutex>
 #include <queue>
@@ -5,28 +7,94 @@
 #include <vector>
 
 class ThreadPool {
-   public:
-    explicit ThreadPool(std::size_t num_threads);
-    ThreadPool();
+public:
+    explicit ThreadPool(std::size_t num_threads) : num_threads_(num_threads) { Init(); }
 
-    ThreadPool(const ThreadPool& other) = delete;
-    ThreadPool& operator=(const ThreadPool& other) = delete;
+    ThreadPool() : num_threads_(std::thread::hardware_concurrency()) {
+        num_threads_ == 0 ? num_threads_ = 1 : num_threads_;
+        Init();
+    }
 
-    ThreadPool(ThreadPool&& other);
-    ThreadPool& operator=(ThreadPool&&);
+    ~ThreadPool() {
+        if (running_)
+            Shutdown();
 
-    ~ThreadPool();
+        for (auto& t : threads_) {
+            if (t.joinable())
+                t.join();
+        }
+    }
 
-    void ExecuteThreadPool();
-    void Shutdown();
-    void LaunchThread();
+    ThreadPool(const ThreadPool&) = delete;
+    ThreadPool& operator=(const ThreadPool&) = delete;
+    ThreadPool(ThreadPool&&) = delete;
+    ThreadPool& operator=(ThreadPool&&) = delete;
+
+    void Init() {
+        for (std::size_t i = 0; i < num_threads_; ++i)
+            threads_.emplace_back(&ThreadPool::LaunchThread, this);
+    }
+
+    void LaunchThread() {
+        while (true) {
+            std::move_only_function<void()> task;
+            {
+                std::unique_lock<std::mutex> lock(mutex_);
+                condition_.wait(lock, [this] { return !running_ || !tasks_.empty(); });
+                if (!running_ && tasks_.empty())
+                    // Ensure all tasks are finished before stopping
+                    return;
+                task = std::move(tasks_.front());
+                tasks_.pop();
+            }
+            task();
+        }
+    }
+
+    void Shutdown() {
+        running_ = false;
+        condition_.notify_all();
+    }
 
     template <typename Fn, typename... Args>
         requires std::invocable<Fn, Args...>
-    std::future<std::invoke_result_t<Fn, Args...>> Enqueue(Fn&& func, Args&&... args);
+    [[nodiscard]] std::future<std::invoke_result_t<Fn, Args...>> Enqueue(Fn&& func, Args&&... args) {
 
-   private:
-    bool running_{true};
+        if (!running_) 
+            throw std::logic_error("Task enqueued after shutdown");
+        
+        using ReturnType = std::invoke_result_t<Fn, Args...>;
+
+        std::packaged_task<ReturnType()> task([f = std::forward<Fn>(func), ... a = std::forward<Args>(args)]() mutable {
+            return std::invoke(f, std::move(a)...);
+        });
+
+        std::future<ReturnType> future = task.get_future();
+
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            tasks_.push([t = std::move(task)] mutable { t(); });
+        }
+
+        condition_.notify_one();
+        return future;
+    }
+
+    template <typename Fn, typename... Args>
+        requires std::invocable<Fn, Args...>
+    void EnqueueDetached(Fn&& func, Args&&... args) {
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            tasks_.push([f = std::forward<Fn>(func), ... a = std::forward<Args>(args)]() mutable {
+                std::invoke(f, std::move(a)...);
+            });
+        }
+
+        condition_.notify_one();
+    }
+
+private:
+    std::atomic<bool> running_{true};
     std::size_t num_threads_{};
     std::vector<std::thread> threads_;
     std::queue<std::move_only_function<void()>> tasks_;

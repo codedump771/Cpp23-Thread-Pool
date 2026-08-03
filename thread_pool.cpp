99954a544e5d673ad1a7c5b922db80692f9dfcc3
PantuflasDev <261576178+PantuflasDev@users.noreply.github.com>
2026-08-03 07:50:11 +0200

    Added enqueue function

--- thread_pool.cpp
+++ thread_pool.cpp
@@ -1,7 +1,10 @@
 #include "thread_pool.hpp"
 
-ThreadPool::ThreadPool(std::size_t num_threads) : num_threads_(num_threads) {
-    ExecuteThreadPool();
+#include <type_traits>
+ThreadPool::ThreadPool(std::size_t num_threads) : num_threads_(num_threads) { ExecuteThreadPool(); }
+
+ThreadPool::ThreadPool() : num_threads_(std::thread::hardware_concurrency()) {
+    if (num_threads_ == 0) num_threads_ = 1;
 }
 
 void ThreadPool::ExecuteThreadPool() {
@@ -12,16 +15,42 @@ void ThreadPool::ExecuteThreadPool() {
 
 void ThreadPool::LaunchThread() {
     while (true) {
-        std::packaged_task<void()> task; 
+        std::move_only_function<void()> task;
         {
-            std::lock_guard<std::mutex> lock(mutex_);
-            if (!running_ && tasks_.empty()) // Ensure all tasks are finished before stopping to run
+            std::unique_lock<std::mutex> lock(mutex_);
+            condition_.wait(lock, [this] { return !running_ || !tasks_.empty(); });
+            if (!running_ && tasks_.empty())
+                // Ensure all tasks are finished before stopping
                 return;
             task = std::move(tasks_.front());
-            tasks_.pop(); 
+            tasks_.pop();
         }
         task();
     }
 }
 
-// I like this project
+template <typename Fn, typename... Args>
+    requires std::invocable<Fn, Args...>
+[[nodiscard]] std::future<std::invoke_result_t<Fn, Args...>> ThreadPool::Enqueue(Fn&& func, Args&&... args) {
+    using ReturnType = std::invoke_result_t<Fn, Args...>;
+
+    std::packaged_task<ReturnType()> task([f = std::forward<Fn>(func), ... a = std::forward<Args>(args)]() mutable {
+        return std::invoke(f, std::move(a)...);
+    });
+
+    std::future<ReturnType> future = task.get_future();
+
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        tasks_.push([t = std::move(task)] mutable { t(); });
+    }
+
+    condition_.notify_one();
+    return future;
+}
+
+void ThreadPool::Shutdown() {
+    for (auto& t : threads_) t.join();
+
+    condition_.notify_all();
+}
--- thread_pool.hpp
+++ thread_pool.hpp
@@ -1,20 +1,36 @@
-#include <thread>
-#include <mutex>
 #include <future>
-#include <vector>
+#include <mutex>
 #include <queue>
+#include <thread>
+#include <vector>
 
 class ThreadPool {
-public: 
+   public:
     explicit ThreadPool(std::size_t num_threads);
+    ThreadPool();
+
+    ThreadPool(const ThreadPool& other) = delete;
+    ThreadPool& operator=(const ThreadPool& other) = delete;
+
+    ThreadPool(ThreadPool&& other);
+    ThreadPool& operator=(ThreadPool&&);
+
+    ~ThreadPool();
+
     void ExecuteThreadPool();
+    void Shutdown();
     void LaunchThread();
-private: 
+
+    template <typename Fn, typename... Args>
+        requires std::invocable<Fn, Args...>
+    std::future<std::invoke_result_t<Fn, Args...>> Enqueue(Fn&& func, Args&&... args);
+
+   private:
     bool running_{true};
     std::size_t num_threads_{};
     std::vector<std::thread> threads_;
-    std::queue<std::packaged_task<void()>> tasks_;
+    std::queue<std::move_only_function<void()>> tasks_;
     std::condition_variable condition_;
-    
+
     mutable std::mutex mutex_;
 };

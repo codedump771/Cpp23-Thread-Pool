ebefface6199bd6534d5413cda096090aec37ed8
PantuflasDev <261576178+PantuflasDev@users.noreply.github.com>
2026-08-04 15:22:12 +0200

    Add wait function

--- thread_pool.hpp
+++ thread_pool.hpp
@@ -1,30 +1,29 @@
 #pragma once
 
+#include <functional>
 #include <future>
 #include <mutex>
 #include <queue>
 #include <thread>
 #include <vector>
 
+// The header uses C++23-only features such as move_only_function.
+// My intention is to add compatiblity with older standards in the future.
+
 class ThreadPool {
 public:
     explicit ThreadPool(std::size_t num_threads) : num_threads_(num_threads) { init(); }
 
-    ThreadPool() : num_threads_(std::thread::hardware_concurrency()) { init(); }
+    // if hardware_concurrency fails, we ensure the amount of threads is atleast one.
+    ThreadPool() : num_threads_(std::max(std::thread::hardware_concurrency(), 1u)) { init(); }
 
-    ~ThreadPool() {
-        shutdown();
-    }
+    ~ThreadPool() { shutdown(); }
 
+    // It doesn't make sense to move or copy a thread pool.
     ThreadPool(const ThreadPool&) = delete;
     ThreadPool& operator=(const ThreadPool&) = delete;
     ThreadPool(ThreadPool&&) = delete;
     ThreadPool& operator=(ThreadPool&&) = delete;
-
-    void init() {
-        for (std::size_t i = 0; i < num_threads_; ++i)
-            threads_.emplace_back(&ThreadPool::launch_thread, this);
-    }
 
     void launch_thread() {
         while (true) {
@@ -38,16 +37,19 @@ public:
                     return;
                 task = std::move(tasks_.front());
                 tasks_.pop();
-                remaining_tasks_--;
             }
             task();
+
+            std::lock_guard lock(mutex_);
+            if (--remaining_tasks_ == 0) {
+                finished_.notify_all();
+            }
         }
     }
 
     void wait() {
-        while (!threads_.empty()) {
-            
-        }
+        std::unique_lock lock(mutex_);
+        finished_.wait(lock, [this] { return !remaining_tasks_; });
     }
 
     void shutdown() {
@@ -55,7 +57,7 @@ public:
             return;
 
         running_.store(false);
-        
+
         condition_.notify_all();
 
         for (auto& t : threads_) {
@@ -75,29 +77,40 @@ public:
 
         std::future<ReturnType> future = task.get_future();
 
+        // We wrap the task inside a lambda for it to be a void function with no arguments.
+        // Since we use move_only_function, we can sumbit the task to the queue directly.
         push_task([f = std::move(task)]() mutable { f(); });
 
         return future;
     }
 
-    template <typename Fn, typename... Args>
-        requires std::invocable<Fn, Args...>
-    void enqueue_void(Fn&& func, Args&&... args) {
-        push_task([f = std::forward<Fn>(func), ... a = std::forward<Args>(args)]() mutable {
-            std::invoke(f, std::move(a)...);
+    template <typename Fn>
+        requires std::invocable<Fn>
+    void enqueue_void(Fn&& func) {
+        push_task([f = std::forward<Fn>(func)]() mutable {
+            f();
         });
     }
 
 private:
+    void init() {
+        for (std::size_t i = 0; i < num_threads_; ++i)
+            threads_.emplace_back(&ThreadPool::launch_thread, this);
+    }
+
     template <typename Fn>
         requires std::invocable<Fn>
     void push_task(Fn&& func) {
-        {
-            std::lock_guard<std::mutex> lock(mutex_);
-            tasks_.push(std::forward<Fn>(func));
-            remaining_tasks_++;
-        }
-        condition_.notify_one();
+            {
+                std::lock_guard<std::mutex> lock(mutex_);
+
+                if (!running_.load())
+                    throw std::logic_error("Task pushed after shutdown");
+    
+                tasks_.push(std::forward<Fn>(func));
+                ++remaining_tasks_;
+            }
+            condition_.notify_one();
     }
 
     std::atomic<bool> running_{true};
@@ -105,7 +118,10 @@ private:
     std::size_t num_threads_{};
     std::vector<std::thread> threads_;
     std::queue<std::move_only_function<void()>> tasks_;
+
     std::condition_variable condition_;
+    std::condition_variable finished_;
 
     mutable std::mutex mutex_;
 };
+
